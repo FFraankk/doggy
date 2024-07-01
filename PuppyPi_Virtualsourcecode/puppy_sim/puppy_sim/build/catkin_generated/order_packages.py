2597f4e0312e1e3bb3917534d712f201558676eb
FFraankk <chenxi.gu2004@outlook.com>
2024-07-02 03:26:43 +0800

    change the dictionary and add gitignore

--- PuppyPi_Virtualsourcecode/puppy_sim/puppy_sim/build/catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = '/home/puppy/puppy_sim/src'
-whitelisted_packages = ''.split(';') if '' != '' else []
-blacklisted_packages = ''.split(';') if '' != '' else []
-underlay_workspaces = '/home/puppy/puppy_sim/devel;/opt/ros/noetic'.split(';') if '/home/puppy/puppy_sim/devel;/opt/ros/noetic' != '' else []
